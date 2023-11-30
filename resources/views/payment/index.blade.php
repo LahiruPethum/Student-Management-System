30a90c33ad1c90dd69122297c5a9d7ba88fb7ae3
Lahiru Pethum <pethum013@gmail.com>
2023-11-30 11:01:55 +0530

    finalize app

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -39,6 +39,7 @@
                                                 {{ csrf_field() }}
                                                 <button type="submit" class="btn btn-danger btn-sm" title="Delete payment" onclick="return confirm('Confirm delete?')"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
                                             </form>
+                                            <!-- <a href="{{ url('report/report1/' . $item->id) }}" title="Edit Payment"><button class="btn btn-primary"><i class="fa fa-print" aria-hidden="true">Print</i></button></a> -->
                         </td>
                     </tr>
                     @endforeach
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\teacherController;
 use App\Http\Controllers\batchController;
 use App\Http\Controllers\EnrollmentController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,4 @@ Route::resource('/courses',CourseController::class);
 Route::resource('/batches',BatchController::class);
 Route::resource('/enrollments',EnrollmentController::class);
 Route::resource('/payments',PaymentController::class);
+
